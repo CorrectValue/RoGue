18d964090a021f4e60a4a2a6a0e3b9266d2cb05e
Lexa <maythe4bewyou@gmail.com>
2020-07-25 22:08:22 +0400

    Added first scripts
    Need to implement the first-person controller

---
+++ RoGue/Assets/Scripts/PlayerStatsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatsController : MonoBehaviour
+{
+    //stores all necessary data about the player
+    //public for now
+    public int hp; //health points
+    public float speed; //movement speed
+    public int money; //amount of money 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //init data
+        hp = 100;
+        speed = 5.0f;
+        money = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if hp is below zero, die
+        if(hp < 1)
+        {
+            die();
+        }
+    }
+
+    void die()
+    {
+        //stop animating and lose player controls
+        //and display an offensive message
+
+    }
+}
